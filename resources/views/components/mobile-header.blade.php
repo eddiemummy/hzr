735b3d78e5ac84ec4092d6e2f90bc700d1eaccc4
Ali Baran Eser <alibaraneser@gmail.com>
2024-06-23 11:40:14 +0300

    menü configured

--- resources/views/components/mobile-header.blade.php
+++ resources/views/components/mobile-header.blade.php
@@ -1,35 +1,12 @@
-<div class="mobile-header-top d-block d-md-none">
-    <div class="container">
-        <div class="row">
-            <div class="col-12">
-                <!-- header-search-mobile start -->
-                <div class="header-search-mobile">
-                    <div class="table">
-                        <div class="table-cell">
-                            <ul>
-                                <li><a class="search-open" href="#"><i class="zmdi zmdi-search"></i></a></li>
-                                <li><a href="login.html"><i class="zmdi zmdi-lock"></i></a></li>
-                                <li><a href="my-account.html"><i class="zmdi zmdi-account"></i></a></li>
-                                <li><a href="wishlist.html"><i class="zmdi zmdi-favorite"></i></a></li>
-                            </ul>
-                        </div>
-                    </div>
-                </div>
-                <!-- header-search-mobile start -->
-            </div>
-        </div>
-    </div>
-</div>
-<header id="sticky-menu" class="header">
+    <header id="sticky-menu" class="header">
     <div class="header-area">
         <div class="container-fluid">
             <div class="row">
                 <div class="col-md-4 offset-md-4 col-7">
                     <div class="logo text-md-center">
-                        <a href="/"><img src="{!! asset('img/logo/logo1.png') !!}" alt=""></a>
+                        <a href="/"><img src="{!! asset('img/logo/logo.png') !!}" alt=""></a>
                     </div>
                 </div>
-
             </div>
         </div>
     </div>
