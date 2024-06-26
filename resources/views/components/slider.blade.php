c6d3de08284b9ee293fdf056b21be5fe4c205b18
Hakan Eser <31434581+eddiemummy@users.noreply.github.com>
2024-06-26 17:14:43 +0300

    new style for images

--- resources/views/components/slider.blade.php
+++ resources/views/components/slider.blade.php
@@ -27,8 +27,8 @@
 					<!-- Slider-banner start -->
 					<div class="slider-banner">
                         @php $prods = \App\Models\Product::latest()->take(2)->get(); @endphp
-                        @foreach($prods as $product)
-                        <div class="single-banner banner-{!! $product->id !!}">
+                        @foreach($prods as $key=>$product)
+                        <div class="single-banner banner-{!! $key !!}">
 							<a class="banner-thumb" href="{!! route('product.single',['slug'=>$product->slug]) !!}"><img src="{!! asset('storage/'.$product->default_image) !!}" alt=""></a>
 							<span class="pro-label new-label">yeni</span>
 							<span class="price">{!! $product->new_price !!}&#8378;</span>
