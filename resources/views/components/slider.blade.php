3e8faeb8efa6632569fd599fdff869714b361e78
Hakan Eser <31434581+eddiemummy@users.noreply.github.com>
2024-06-26 16:51:28 +0300

    box-plot v6

--- resources/views/components/slider.blade.php
+++ resources/views/components/slider.blade.php
@@ -32,7 +32,7 @@
 							<a class="banner-thumb" href="{{route('product.single',['slug'=>$product->slug])}}"><img src="{!! asset('storage/'.$product->default_image) !!}" alt=""></a>
 							<span class="pro-label new-label">yeni</span>
 							<span class="price">{!! $product->new_price !!}&#8378;</span>
-							<div class="banner-brief flex-lg-row">
+							<div class="flex-lg-row">
 								<h2 class="banner-title"><a href="{{route('product.single',['slug'=>$product->slug])}}">{!! $product->name !!}</a></h2>
 								<p class="mb-0">{!! $product->category->name !!}</p>
 							</div>
